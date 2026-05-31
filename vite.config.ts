44f09638451b9a303050790231413b15d8fa38fb
Philipp Schuler <p.schuler@durchblicker.at>
2026-05-31 12:24:09 +0200

    refactor: restructure Vite config for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,27 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@mdx-js/rollup";
 import remarkGfm from "remark-gfm";
-
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [{
-    enforce: "pre",
-    ...mdx({
-      remarkPlugins: [remarkGfm], // <--- 2. Hier aktivieren
-    }),
-  }, react(), tailwindcss(), cloudflare()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    {
+      enforce: "pre",
+      ...mdx({
+        remarkPlugins: [remarkGfm],
+      }),
+    },
+    react(),
+    tailwindcss(),
+    cloudflare(),
+  ],
   base: "/",
-});
+  define: {
+    "import.meta.env.VITE_API_URL": JSON.stringify(
+      mode === "production"
+        ? "https://ankilm-backend-api-j2mn4yc65q-ey.a.run.app"
+        : "http://localhost:8080"
+    ),
+  },
+}));
